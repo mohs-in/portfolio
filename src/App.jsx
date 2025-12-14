dc900dab33237130f345cc0e0ca9b528b5ef7f8f
Mohsin <in.mohsin@outlook.com>
2025-12-14 23:45:42 +0530

    fix: dependencies, configs and urls for deploy(gh-pages)

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       }}
     >
       <Routes>
-        <Route path='/' element={<Hero /> }/>
+        <Route path='/portfolio' element={<Hero /> }/>
       </Routes>
     </BrowserRouter>
   )
